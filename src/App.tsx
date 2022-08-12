d290ae3072271f544cdb90f0d4d0d3fa6d4bd334
Yann Donazzolo <ydz0l038@gmail.com>
2022-08-12 17:48:04 +0200

    setup functionnal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { FormEvent, useEffect, useState } from "react";
 import "./App.css";
+import { FormEvent, useEffect, useState } from "react";
 import { addMenuItem, listMenuItems, removeMenuItem } from "./services/menu";
 
 function App() {
   const [newMenuItem, setNewMenuItem] = useState("");
   const [loading, setLoading] = useState(false);
   const [menuItems, setMenuItems] = useState<string[]>([]);
+  const [currentOrder, setCurrentOrder] = useState<Record<string, number>>({});
 
   const onChangeNewMenuItem = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewMenuItem(e.target.value);
@@ -25,6 +26,37 @@ function App() {
     removeMenuItem(menuItem);
     setMenuItems(listMenuItems());
     setLoading(false);
+  };
+
+  const handleAddToOrder = (newItemCommand: string) => {
+    setCurrentOrder((prev) => {
+      if (prev[newItemCommand]) {
+        return {
+          ...prev,
+          [newItemCommand]: prev[newItemCommand] + 1,
+        };
+      } else {
+        return {
+          ...prev,
+          [newItemCommand]: 1,
+        };
+      }
+    });
+  };
+
+  const handleRemoveFromOrder = (itemToRemove: string) => {
+    setCurrentOrder((prev) => {
+      if (prev[itemToRemove] === 1) {
+        const newOrder = { ...prev };
+        delete newOrder[itemToRemove];
+        return newOrder;
+      } else {
+        return {
+          ...prev,
+          [itemToRemove]: prev[itemToRemove] - 1,
+        };
+      }
+    });
   };
 
   useEffect(() => {
@@ -47,15 +79,24 @@ function App() {
           />
           <button type="submit">Add</button>
         </form>
-        <ul>
+        <div>
           {menuItems.map((item) => (
-            <li key={item} onClick={() => handleRemoveMenuItem(item)}>
-              {item}
-            </li>
+            <div key={item}>
+              <button onClick={() => handleAddToOrder(item)}>{item}</button>
+              <button onClick={() => handleRemoveMenuItem(item)}>Delete</button>
+            </div>
           ))}
-        </ul>
+        </div>
       </section>
-      <section className="command"></section>
+      <section className="command">
+        <h2>Current command</h2>
+        {Object.keys(currentOrder).map((item) => (
+          <div key={item}>
+            {item} {currentOrder[item]}
+            <button onClick={() => handleRemoveFromOrder(item)}>remove</button>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
